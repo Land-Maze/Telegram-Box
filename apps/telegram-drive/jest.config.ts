fa4ae87c96b931cb6e4fc82d1c4d49afa676f8d5
Timur Dzhesur <timurjes@gmail.com>
2024-02-03 04:42:02 +0100

    Initial commit

---
+++ apps/telegram-drive/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'telegram-drive',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/telegram-drive',
+};
